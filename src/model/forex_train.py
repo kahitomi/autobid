f6c94ba80c34b866fe59e6567de7854ba22197bf
kahitomi <1034410104@qq.com>
2016-08-30 20:48:36 +0800

    test

--- src/model/forex_train.py
+++ src/model/forex_train.py
@@ -82,8 +82,8 @@ tf.app.flags.DEFINE_float("learning_rate_decay_factor", 0.99, "Learning rate dec
 
 tf.app.flags.DEFINE_float("max_gradient_norm", 5.0, "Clip gradients to this norm.")
 
-tf.app.flags.DEFINE_integer("batch_size", 10, "Batch size to use during training.")
-# tf.app.flags.DEFINE_integer("batch_size", 100, "Batch size to use during training.")
+# tf.app.flags.DEFINE_integer("batch_size", 10, "Batch size to use during training.")
+tf.app.flags.DEFINE_integer("batch_size", 100, "Batch size to use during training.")
 
 tf.app.flags.DEFINE_integer("size", 200, "Size of each model layer.")
 
