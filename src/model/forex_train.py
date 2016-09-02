22f5983b8f088f18e7ee3cc5336073faf82d7142
kahitomi <1034410104@qq.com>
2016-09-02 20:26:03 +0800

    value test

--- src/model/forex_train.py
+++ src/model/forex_train.py
@@ -63,7 +63,7 @@ BASE_LENGTH = 60 # seconds
 NUMBER_SPLIT = 50
 BASIC_SPLIT = 0.00001
 
-IFSAVE = False
+IFSAVE = True
 IFTEST = False
 
 VOLUME = [99999999, 0]
@@ -89,7 +89,7 @@ tf.app.flags.DEFINE_float("learning_rate_decay_factor", 0.99, "Learning rate dec
 tf.app.flags.DEFINE_float("max_gradient_norm", 5.0, "Clip gradients to this norm.")
 
 # tf.app.flags.DEFINE_integer("batch_size", 10, "Batch size to use during training.")
-tf.app.flags.DEFINE_integer("batch_size", 20, "Batch size to use during training.")
+tf.app.flags.DEFINE_integer("batch_size", 10, "Batch size to use during training.")
 
 tf.app.flags.DEFINE_integer("size", 100, "Size of each model layer.")
 
@@ -104,8 +104,8 @@ tf.app.flags.DEFINE_string("train_dir", "src/model/forex/"+SAVE_NAME, "Training 
 
 tf.app.flags.DEFINE_integer("max_train_data_size", 0, "Limit on the size of training data (0: no limit).")
 
-# tf.app.flags.DEFINE_integer("steps_per_checkpoint", 10000, "How many training steps to do per checkpoint.")
-tf.app.flags.DEFINE_integer("steps_per_checkpoint", 10, "How many training steps to do per checkpoint.")
+tf.app.flags.DEFINE_integer("steps_per_checkpoint", 16100, "How many training steps to do per checkpoint.")
+# tf.app.flags.DEFINE_integer("steps_per_checkpoint", 10, "How many training steps to do per checkpoint.")
 
 tf.app.flags.DEFINE_boolean("decode", False, "Set to True for interactive decoding.")
 tf.app.flags.DEFINE_boolean("self_test", False, "Run a self-test if this is set to True.")
