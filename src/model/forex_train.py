83e4a04b97af77ce001fb56860a43f758a0d2aec
kahitomi <1034410104@qq.com>
2016-09-03 14:48:38 +0800

    value test

--- src/model/forex_train.py
+++ src/model/forex_train.py
@@ -110,7 +110,7 @@ tf.app.flags.DEFINE_string("train_dir", "src/model/forex/"+SAVE_NAME, "Training 
 tf.app.flags.DEFINE_integer("max_train_data_size", 0, "Limit on the size of training data (0: no limit).")
 
 # tf.app.flags.DEFINE_integer("steps_per_checkpoint", 16100, "How many training steps to do per checkpoint.")
-tf.app.flags.DEFINE_integer("steps_per_checkpoint", 80, "How many training steps to do per checkpoint.")
+tf.app.flags.DEFINE_integer("steps_per_checkpoint", 800, "How many training steps to do per checkpoint.")
 
 tf.app.flags.DEFINE_boolean("decode", False, "Set to True for interactive decoding.")
 tf.app.flags.DEFINE_boolean("self_test", False, "Run a self-test if this is set to True.")
@@ -178,7 +178,7 @@ def read_data(source_path, max_size=None, test=None):
 		###########
 		# FOR TEST
 		###########
-		if IFTEST and counter > ((bucket[0]+bucket[1])*BASE_LENGTH/DATA_DIS+800):
+		if IFTEST and counter > ((bucket[0]+bucket[1])*BASE_LENGTH/DATA_DIS+8000):
 			break
 
 
