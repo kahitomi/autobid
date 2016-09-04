c1d34f1b06362a01215a7a198e3e344ed9c4d440
kahitomi <1034410104@qq.com>
2016-09-04 14:23:54 +0800

    value test

--- src/model/forex_train.py
+++ src/model/forex_train.py
@@ -109,7 +109,7 @@ tf.app.flags.DEFINE_string("train_dir", "src/model/forex/"+SAVE_NAME, "Training 
 
 tf.app.flags.DEFINE_integer("max_train_data_size", 0, "Limit on the size of training data (0: no limit).")
 
-tf.app.flags.DEFINE_integer("steps_per_checkpoint", 16100, "How many training steps to do per checkpoint.")
+tf.app.flags.DEFINE_integer("steps_per_checkpoint", 16000, "How many training steps to do per checkpoint.")
 # tf.app.flags.DEFINE_integer("steps_per_checkpoint", 800, "How many training steps to do per checkpoint.")
 
 tf.app.flags.DEFINE_boolean("decode", False, "Set to True for interactive decoding.")
@@ -770,7 +770,7 @@ def self_test():
 
 		# 24 picese wheel
 		data_size = len(test_set)
-		wheel_part_number = 2
+		wheel_part_number = 7
 		wheel = [ (data_size/wheel_part_number)*(x+1) for x in range(wheel_part_number)]
 
 
