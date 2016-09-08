d874be2bf16446293c33fe788c63b1a56c506913
kahitomi <1034410104@qq.com>
2016-09-08 23:39:32 +0800

    value test

--- src/model/forex_train.py
+++ src/model/forex_train.py
@@ -113,7 +113,7 @@ tf.app.flags.DEFINE_integer("max_train_data_size", 0, "Limit on the size of trai
 
 # tf.app.flags.DEFINE_integer("steps_per_checkpoint", 16000, "How many training steps to do per checkpoint.")
 
-tf.app.flags.DEFINE_integer("steps_per_checkpoint", 3400, "How many training steps to do per checkpoint.")
+tf.app.flags.DEFINE_integer("steps_per_checkpoint", 2800, "How many training steps to do per checkpoint.")
 
 tf.app.flags.DEFINE_boolean("decode", False, "Set to True for interactive decoding.")
 tf.app.flags.DEFINE_boolean("self_test", False, "Run a self-test if this is set to True.")
